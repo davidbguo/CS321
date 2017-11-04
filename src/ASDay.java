f5866e6b1cb37c1df4440b88df937e34c6966d53
david guo <davidbguo@gmail.com>
2017-11-04 13:49:43 -0400

    new code on open time slots left in day

--- src/ASDay.java
+++ src/ASDay.java
@@ -9,13 +9,14 @@ public class ASDay {
 	protected LocalDate date;
 	protected double hoursLeft;
 	protected double hoursWorking;
+    protected static final double MINIMUM_TASK_BLOCK_LENGTH = 0.25; //in hours
     
     public ASDay(LocalDate day) {
     	this.date = day;
     	hoursLeft = 24;
     	hoursWorking = 0;
     	eventsOfDay.addFirst(new SubTask("startOfDay", LocalTime.of(0, 0),LocalTime.of(0, 0)));
-    	eventsOfDay.addLast(new SubTask("endofDay", LocalTime.of(23, 59),LocalTime.of(23, 59)));
+    	eventsOfDay.addLast(new SubTask("endOfDay", LocalTime.of(23, 59),LocalTime.of(23, 59)));
     	updateHoursLeft();
     	
     }
@@ -23,7 +24,6 @@ public class ASDay {
     public void updateHoursLeft() {
     	double tempHoursLeft = 0;
     	double tempHoursWorking = 0;
-    	
     	LocalTime tempEnd = eventsOfDay.getFirst().endTime;
     	LocalTime tempStart = eventsOfDay.getFirst().endTime;
     	for(int i = 1; i< eventsOfDay.size(); i++) {
@@ -35,7 +35,24 @@ public class ASDay {
     	hoursLeft = tempHoursLeft;
     	hoursWorking = tempHoursWorking;
     }
-    
+
+    public ArrayList<LocalTime> getOpenTimeSlot(){
+        ArrayList<LocalTime> openTimeSlot= new ArrayList<LocalTime>();
+        LocalTime tempEnd = eventsOfDay.getFirst().endTime;
+    	LocalTime tempStart = eventsOfDay.get(1).startTime;
+    	for(int i = 1; i< eventsOfDay.size(); i++) { 
+            if(tempEnd.until(tempStart,ChronoUnit.HOURS)>MINIMUM_TASK_BLOCK_LENGTH) {
+                //if there is more than 15 mins left 
+                openTimeSlot.add(tempEnd);
+                openTimeSlot.add(tempStart);
+            }
+            if(eventsOfDay.get(i).name.equals("endOfDay"))  
+                break;
+            tempEnd = eventsOfDay.get(i).endTime;
+            tempStart = eventsOfDay.get(i+1).startTime;
+    	}      
+        return openTimeSlot;        
+    }
     public String[] getDailyEventsDetails(){
     	return details;
     }
