a8c0c9cd50810026e57336f8e5b3c2b7aae3a79e
Key Leutchit <kleutchit@gmail.com>
2017-11-05 18:32:42 -0500

    Added TestDriver.java

---
+++ src/TestDriver.java
@@ -0,0 +1,28 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+
+public class TestDriver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+			
+		LocalDate day = LocalDate.of(2015, 12, 31);
+		ASDay test = new ASDay(day);
+		SubTask	s1 = new SubTask("test",LocalTime.of(13, 0),LocalTime.of(14,00));
+		SubTask	s2 = new SubTask("test",LocalTime.of(14, 15),LocalTime.of(15,00));
+			/*
+				SubTask	s3 = new SubTask("test",LocalTime.of(13, 0),LocalTime.of(14,00));
+				SubTask	s4 = new SubTask("test",LocalTime.of(13, 0),LocalTime.of(14,00));
+				SubTask	s5 = new SubTask("test",LocalTime.of(13, 0),LocalTime.of(14,00));
+			*/
+		test.eventsOfDay.add(s1);
+		test.eventsOfDay.add(s2);
+		System.out.print(test.getOpenTimeSlot());
+		
+
+	}
+
+}
