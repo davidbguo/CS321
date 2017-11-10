9bb66fa6027900f043ca874142bbe0f5eaa0c73d
Key Leutchit <kleutchit@gmail.com>
2017-11-10 14:47:09 -0500

    update to TaskType

--- src/TaskTypeEnum.java
+++ src/TaskTypeEnum.java
@@ -1,5 +1,15 @@
 
 public enum TaskTypeEnum {
+	TOPIC_RESEARCH,
+	READING,
+	PRESENTATION,
+	PROBLEM_SET,
+	ESSAY_LONG,
+	ESSAY_SHORT,
+	QUEST,
+	QUIZ,
+	TEST,
+	STUDY,
 	PROBLEMSET,
 	HOMEWORK,
 	ESSAY,
@@ -13,13 +23,43 @@ public enum TaskTypeEnum {
 	public int getHourFactor(){
 		int s;
 		switch(this){
-			case PROBLEMSET:
+			case TOPIC_RESEARCH:
+				s = 4;
+				break;
+			case READING:
+				s = 4;
+				break;
+			case PRESENTATION:
+				s = 4;
+				break;
+			case PROBLEM_SET:
+				s = 4;
+				break;
+			case ESSAY_LONG:
+				s = 4;
+				break;
+			case ESSAY_SHORT:
+				s = 4;
+				break;
+			case QUEST:
+				s = 4;
+				break;
+			case QUIZ:
+				s = 4;
+				break;
+			case TEST:
+				s = 4;
+				break;
+			case STUDY:
 				s = 4;
 				break;
 			case HOMEWORK:
 				s = 4;
 				break;
 			case ESSAY:
+				s = 4;
+				break;
+			case PROBLEMSET:
 				s = 4;
 				break;
 			case PROJECT:
